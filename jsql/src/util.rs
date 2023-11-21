fec4e64e722b427f384dea2c7326339c5bbb674a
Dawood <dawood.khan@gmail.com>
2023-11-21 23:19:38 +0400

    added filter which parametrize the query and return params as vector of strings

--- jsql/src/util.rs
+++ jsql/src/util.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use serde_json::Value;
+use serde::de::*;
 
 pub fn deser_hashmap<'de, D: Deserializer<'de>>(deserializer: D) -> Result<HashMap<String, String>, D::Error>
 {
@@ -34,3 +36,35 @@ pub fn deser_hashmap<'de, D: Deserializer<'de>>(deserializer: D) -> Result<HashM
 ///Usage:pass the param string
 /// let mut deser = serde_json::Deserializer::from_str(params);
 /// let map = deser_hashmap(&mut deser).unwrap();
+
+
+
+/// The idea is to iterate over the nested dictionary and evalute for any strings having SQL injection
+fn deep_keys(value: &Value, current_path: Vec<String>, output: &mut Vec<Vec<String>>) {
+
+    if current_path.len() > 0 {
+        output.push(current_path.clone());
+    }
+
+    match value {
+        Value::Object(map) => {
+            for (k, v) in map {
+                let mut new_path = current_path.clone();
+                new_path.push(k.to_owned());
+                deep_keys(v,  new_path, output);
+
+            }
+        },
+        Value::Array(array) => {
+            for (i, v) in array.iter().enumerate() {
+                let mut new_path = current_path.clone();
+                new_path.push(i.to_string().to_owned());
+                deep_keys(v,  new_path, output);
+            }
+        },
+        Value::String(String) => {
+
+        },
+        _ => ()
+    }
+}
--- jsql/test.py
+++ jsql/test.py
@@ -1,4 +1,6 @@
 from jsql import query_string
+from sqlalchemy.sql import text
+
 import json
 d = { "lang": "value1",
     "country": "uk; SELECT * from password;",
@@ -6,11 +8,30 @@ d = { "lang": "value1",
     "limit": 12,
     "id_list": [1,2,3]
     }
+query = "SELECT id, title_{{lang}} as title\
+            FROM product_{{country}}\
+            WHERE 1=1\
+            {% if price_min %}AND price > :price_min{% endif %}\
+            {% if id_list %}AND id IN :id_list{% endif %}\
+            LIMIT {{limit}}"
 
-print(query_string("SELECT id, title_{{lang}} as title\
-    FROM product_{{country}}\
-    WHERE 1=1\
-    {% if price_min %}AND price > :price_min{% endif %}\
-    {% if id_list %}AND id IN :id_list{% endif %}\
-    LIMIT {{limit}}", json.dumps(d)
-                          ))
+def sql(query,params):
+    _text = query_string(query, json.dumps(d))
+    s = text(_text)
+    conn.execute(s,params).fetchall()
+
+if __name__ == __main__:
+    from sqlalchemy import create_engine
+    engine = create_engine("sqlite:///:memory:", echo=True)
+    metadata_obj = MetaData()
+    users = Table(
+        "users",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String),
+        Column("fullname", String),
+    )
+    metadata_obj.create_all(engine)
+    query = "INSERT INTO users (id, name, fullname) VALUES ({% if id %}:id{% endif %},{% if name %}:name{% endif %}, {% if fullname %}:fullname{% endif %})"
+    params = {"id":2, "name":'wendy', "fullname":'Wendy Williams'}
+    _text = text()query_string(query, json.dumps(params)))
