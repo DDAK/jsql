5df6c75b558309160ccc4b41cf3ea2c61ce52ad6
Dawood <dawood.khan@gmail.com>
2023-11-22 00:20:45 +0400

    updated folder struct

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from jsql import query_string
+from jsql import sql
+
 import json
 d = { "lang": "value1",
     "country": "uk; SELECT * from password;",
@@ -6,11 +7,31 @@ d = { "lang": "value1",
     "limit": 12,
     "id_list": [1,2,3]
     }
+query = "SELECT id, title_{{lang}} as title\
+                 FROM product_{{country}}\
+                 WHERE 1=1\
+                 {% if price_min %}AND price > {{price_min|bind}}{% endif %}\
+                 {% if id_list %}AND id IN {{id_list | InClause}} {% endif %}\
+                 LIMIT {{limit|bind}}"
 
-print(query_string("SELECT id, title_{{lang}} as title\
-    FROM product_{{country}}\
-    WHERE 1=1\
-    {% if price_min %}AND price > :price_min{% endif %}\
-    {% if id_list %}AND id IN :id_list{% endif %}\
-    LIMIT {{limit}}", json.dumps(d)
-                          ))
+def _sql(query,params):
+    return sql(query, json.dumps(d))
+
+
+print(_sql(query,d))
+#
+# if __name__ == __main__:
+#     from sqlalchemy import create_engine
+#     engine = create_engine("sqlite:///:memory:", echo=True)
+#     metadata_obj = MetaData()
+#     users = Table(
+#         "users",
+#         metadata_obj,
+#         Column("id", Integer, primary_key=True),
+#         Column("name", String),
+#         Column("fullname", String),
+#     )
+#     metadata_obj.create_all(engine)
+#     query = "INSERT INTO users (id, name, fullname) VALUES ({% if id %}:id{% endif %},{% if name %}:name{% endif %}, {% if fullname %}:fullname{% endif %})"
+#     params = {"id":2, "name":'wendy', "fullname":'Wendy Williams'}
+#     _text = text(query_string(query, json.dumps(params)))
